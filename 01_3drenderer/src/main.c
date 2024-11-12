e06965e2315990f7d8cc53719361a9dd6db99b98
Aeshma <madhushanka2727@gmail.com>
2024-11-12 19:14:01 +0530

    populate cube points array

--- 01_3drenderer/src/main.c
+++ 01_3drenderer/src/main.c
@@ -5,6 +5,12 @@
 #include "display.h"
 #include "vector.h"
 
+//////////////////////////////////////////////////////////////
+// declare a array of points/vectors
+//////////////////////////////////////////////////////////////
+const int NUMBER_OF_POINTS = 9 * 9 * 9; // 9 * 9 * 9 cube
+vec3_t cube_points[NUMBER_OF_POINTS];
+
 bool isRunning = false;
 
 void setup(void) {
@@ -21,6 +27,20 @@ void setup(void) {
         window_width,
         window_height
     );
+
+    // starting loading array of vectors
+    // from -1 to 1 , 9 * 9 * 9 cube
+    int point_count = 0;
+
+    for (float x = -1; x < 1; x += 0.25) {
+        for (float y = -1; y < 1; y += 0.25) {
+            for (float z = -1; z < 1; z += 0.25) {
+                vec3_t new_point = { .x = x, .y = y, .z = z };
+                cube_points[point_count++] = new_point;
+            }
+        }
+    }
+    
 }
 
 void process_input(void) {
